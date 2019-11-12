6a533009098e6cc56cd433931f669873d39e50da
jlad2017 <jlad2017@mymail.pomona.edu>
2019-11-11 22:05:00 -0800

    remove repeated headers

--- mine_tweets.py
+++ mine_tweets.py
@@ -10,13 +10,13 @@ def main(argv):
     'wework ipo', and saves the Pandas dataframe to a .csv file
     """ 
 
-    rapid_api_key = 'xxxxx'
+    rapid_api_key = '568b853ebamshaad79576249ddcbp1921f7jsn634271203771'
 
     twitter_keys = {
-        'consumer_key':         'xxxxx',
-        'consumer_secret':      'xxxxx',
-        'access_token_key':     'xxxxx',
-        'access_token_secret':  'xxxxx'
+        'consumer_key':         'ytF42avewxOOM3sIfesKzkbAZ',
+        'consumer_secret':      'ZOHNaPfPYT6K2toYqRgI2GjsXBy5klIHU8xERfXAOrLPIH6KBw',
+        'access_token_key':     '1019863774101061632-ELZ22Ze3iCi12nSuVhPh60EkzTZLra',
+        'access_token_secret':  'YDIWTC4XkcwKXowE00VTykZe3Jjxe8XVYnkLdy94u1ld4'
     }
 
     miner = tweepy_miner_class.TweetMiner(keys_dict=twitter_keys, result_limit=100)
@@ -33,8 +33,8 @@ def main(argv):
 
             print('scraping for day ' + date_list[i+1])
 
-            data = miner.mine_queried_tweets_old('wework', since=date_list[i+1], until=date_list[i], max=200)
-            data.to_csv(f, sep='\t', encoding='utf-8', index=False, header=f.tell()==0)
+            data = miner.mine_queried_tweets_old('wework', since=date_list[i+1], until=date_list[i], max=500)
+            data.to_csv(f, sep='\t', encoding='utf-8', index=False, mode='a', header=f.tell()==0)
 
     print('done!')
 
