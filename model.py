8be6a3683576e6c8439fa90e180e621a770cc8f4
Marcos Acosta <macosta1625@gmail.com>
2019-11-25 16:30:42 -0800

    Added word to vector and created dataframe containing vectors for modeling

--- model.py
+++ model.py
@@ -1,18 +1,44 @@
 from gensim.models import FastText
 import pandas as pd
 
-training_data = pd.read_csv("trainingandtestdata/cleanedsentiment.csv")
+# Get labeled data
+training_data = pd.read_csv("trainingandtestdata/cleaned_training_data_1.csv")
+# Sentences and sentiments
 sentences = training_data['text'].tolist()
+sentiments = training_data['sentiment'].tolist()
+# Create a new cleaner dataframe to holt text, vector, and correct sentiment
+cleaned = pd.DataFrame(columns=['text','vector','sentiment'])
+cleaned['text'] = sentences
+cleaned['sentiment'] = sentiments
 
-def train_model():
+# Load pre-trained fasttext model
+model = FastText.load('models/fasttext_model')
+
+# Train a fasttext model
+def train_model(fname):
     # instantiate model
-    model = FastText(size=5, window=3, min_count=1)
+    model = FastText(size=4, window=3, min_count=1)
     # add vocabulary and train
     model.build_vocab(sentences=sentences)
     model.train(sentences=sentences, total_examples=len(sentences), epochs=10)
+    model.save(fname)
     return model
 
+# Add word vectors in a sentence
+def sum_vectors(sent):
+    vector = [0.0, 0.0, 0.0, 0.0]
+    for token in sent:
+        vector += model.wv[token]
+    return vector.tolist()
 
-model = train_model()
-print(model.wv['shit'])
-print(model.wv['hate'])
+# Add sentence vectors to cleaned
+def convert_sentences_to_vecs():
+    vectors = []
+    for sent in sentences:
+        vectors.append(sum_vectors(sent))
+    cleaned['vector'] = vectors
+
+convert_sentences_to_vecs()
+cleaned.to_csv('trainingandtestdata/training_vectors.csv')
+
+
