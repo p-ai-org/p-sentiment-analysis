2c9e3995e05e1f9deb838cc8f52e8691d343c7b5
AlexKer <alexker@crescentschool.org>
2019-12-11 20:30:09 -0800

    hello

--- sentiment_model.py
+++ sentiment_model.py
@@ -8,44 +8,56 @@ from tensorflow.keras.layers import Dense, Dropout, Activation, Embedding, LSTM,
 from sklearn.model_selection import train_test_split
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import GridSearchCV
+from imblearn.over_sampling import SMOTE
+
 
 """
 WORK IN PROGRESS
 """
 
 # Get data
-data = pd.read_csv("trainingandtestdata/spread_training_vectors.csv")
+df = pd.read_csv("trainingandtestdata/spread_training_vectors_complete.csv")
 # Remove quotations from vector lists (result of converting lists to csv)
 # data['vector'] = data['vector'].apply(ast.literal_eval)
 
-X_train = data.loc[:, 'v0':'v99']
-target = data['sentiment']
+df = df.reindex(np.random.permutation(df.index))
+train, test = train_test_split(df, test_size=0.10)
+# X features are set to all columns excluding the label
+X_train = train[train.columns.difference(['sentiment'])]
+y_train = train['sentiment']
+X_test = test[train.columns.difference(['sentiment'])]
+y_test = test['sentiment']
 
-X_train, X_valid, y_train, y_valid = train_test_split(X_train, target, random_state=0)
+def perform_smote(X_train, y_train):
+    smote = SMOTE('minority')
+    X_sm, y_sm = smote.fit_sample(X_train, y_train)
+    return X_sm, y_sm
 
-print('X train:')
-print(X_train.shape)
-print('y train:')
-print(y_train.shape)
+X_train, y_train = perform_smote(X_train, y_train)
+
+print(len(y_train[y_train==0]))
+print(len(y_train[y_train==1]))
+print(len(y_train[y_train==2]))
 
 def reshape_for_1DCNN(X):
     return np.expand_dims(X, axis=2)
 X_train = reshape_for_1DCNN(X_train)
+X_test = reshape_for_1DCNN(X_test)
 
-def build_1DCNN(dropout_rate=0.0):
+def run_1DCNN():
     print('-----------Running 1D CNN-----------')
     # Build model 
     model = Sequential()
-    model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(100, 1)))
-    model.add(Dropout(dropout_rate))
-    model.add(Conv1D(filters=128, kernel_size=3, activation='relu'))
-    model.add(Dropout(dropout_rate))
+    model.add(Conv1D(filters=128, kernel_size=3, activation='relu', input_shape=(250, 1)))
+    model.add(Dropout(0.2))
+    model.add(Conv1D(filters=256, kernel_size=3, activation='relu'))
+    model.add(Dropout(0.2))
     model.add(MaxPooling1D(pool_size=3))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
-    model.add(Dense(4, activation='softmax'))
+    model.add(Dense(3, activation='softmax'))
     # Choose optimizer and loss function
-    opt = tf.keras.optimizers.Adam(lr=0.001, decay=1e-6)
+    opt = tf.keras.optimizers.Adam(lr=0.01, decay=1e-6)
     loss = 'sparse_categorical_crossentropy'
     # Compile 
     model.compile(optimizer=opt, 
@@ -53,55 +65,39 @@ def build_1DCNN(dropout_rate=0.0):
         metrics=['acc'])
     # Fit on training data and cross-validate
     # Test on testing data
-    return model
+    model.fit(X_train, y_train,
+        epochs=15,
+        batch_size=128)
+    # Test on testing data
+    score = model.evaluate(X_test, y_test, batch_size=128)
 
-model = KerasClassifier(build_fn=build_1DCNN)
+run_1DCNN()
+
+
+
+
+
+
+
+
+# model = KerasClassifier(build_fn=build_1DCNN)
 # define the grid search parameters
-batch_size = [5, 10, 20]
-epochs = [10, 15]
-dropout_rate = [0.01, 0.1, 0.2]
-param_grid = dict(batch_size=batch_size, epochs=epochs, dropout_rate=dropout_rate)
-grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=3)
-grid_result = grid.fit(X_train, y_train)
-# summarize results
-print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-means = grid_result.cv_results_['mean_test_score']
-stds = grid_result.cv_results_['std_test_score']
-params = grid_result.cv_results_['params']
-for mean, stdev, param in zip(means, stds, params):
-    print("%f (%f) with: %r" % (mean, stdev, param))
+# batch_size = [100]
+# epochs = [10]
+# dropout_rate = [0.1]
 
 
+# param_grid = dict(batch_size=batch_size, epochs=epochs, dropout_rate=dropout_rate)
+# grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=3)
+# grid_result = grid.fit(X_train, y_train)
+# # summarize results
+# print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+# means = grid_result.cv_results_['mean_test_score']
+# stds = grid_result.cv_results_['std_test_score']
+# params = grid_result.cv_results_['params']
+# for mean, stdev, param in zip(means, stds, params):
+#     print("%f (%f) with: %r" % (mean, stdev, param))
 
-
-
-
-
-""" LSTM """
-
-def create_lstm_model(embed_dim, lstm_out, batch_size):
-    model = Sequential()
-    model.add(Embedding(2500, embed_dim, input_length = 100, dropout = 0.2))
-    model.add(LSTM(lstm_out, dropout = 0.2, recurrent_dropout = 0.2))
-    model.add(Dense(1,activation='softmax'))
-    model.compile(loss = 'sparse_categorical_crossentropy', optimizer='adam', metrics = ['accuracy'])
-    print(model.summary())
-    return model
-
-""" SEQUENTIAL """
-
-def create_simple_model():
-    model = Sequential()
-    model.add(Dense(12, input_dim=100, activation='relu'))
-    model.add(Dense(256, activation='relu'))
-    model.add(Dense(3, activation='softmax'))
-    model.compile(loss = 'sparse_categorical_crossentropy', optimizer='adam', metrics = ['accuracy'])
-    print(model.metrics_names)
-    print(model.summary())
-    return model
-
-# model = create_lstm_model(128, 200, 32)
-# model = create_simple_model()
 # model.fit(X_train, y_train, batch_size = 50, epochs = 25,  verbose = 5)
 # score = model.evaluate(X_valid, y_valid, 50)
 # print(score)
